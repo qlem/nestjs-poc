eb56656dac04f6e1b045719998546c3f27916323
Clément Dommerc <clement.dommerc@gmail.com>
2021-06-28 20:15:16 +0200

    wip

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -1,10 +1,12 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
+import { logger } from '../utils';
+
 @Injectable()
 class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log(`${req.method} - ${req.originalUrl}`);
+    logger.info(`${req.method} - ${req.originalUrl}`);
     next();
   }
 }
