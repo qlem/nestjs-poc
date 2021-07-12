b4a73dc08feaac53106951b3a2c8ec6fe68daa13
Clément Dommerc <clement.dommerc@gmail.com>
2021-07-12 18:02:42 +0200

    remove dist folder

--- api-gateway/dist/middlewares/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.queryLogger = void 0;
-const queryLogger_1 = __importDefault(require("./queryLogger"));
-exports.queryLogger = queryLogger_1.default;
-//# sourceMappingURL=index.js.map
